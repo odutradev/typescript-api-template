e8f5a7ebecba92077d16d081a99a41a9608700d7
odutradev <joao.vitornl@gmail.com>
2026-03-09 17:07:53 -0300

    feat: Changing routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,6 @@ import { Router } from "express";
 
 import controlAccess from "@middlewares/controlAccess";
 import usersRouter from "./resources/users.router";
-import adminRouter from "./resources/admin.router";
 import hasAdmin from "@middlewares/hasAdmin";
 import auth from "@middlewares/auth";
 
@@ -16,7 +15,6 @@ router.get("/validate/control-access", controlAccess, (req, res) => {
     res.sendStatus(200);
 });
 
-router.use("/admin", [controlAccess, auth, hasAdmin],  adminRouter);
 router.use("/users", [controlAccess],  usersRouter);
 
 export default router;
